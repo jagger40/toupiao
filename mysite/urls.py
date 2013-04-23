b2bc3c00ed00b82c3003f1daebc35e90cae0d25d
YunLZheng <yunl.zheng@gmail.com>
2013-04-23 16:05:19 +0800

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
 import logging
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 # Uncomment the next two lines to enable the admin:
@@ -11,5 +13,9 @@ urlpatterns = patterns('',
   
   url(r'^polls/',include('polls.urls')),
   url(r'^admin/',include(admin.site.urls)),
-  url(r'^static/(?P<path>.*)$','django.views.static.serve',document_root:settings.MEDIA_ROOT),
+
 )
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
