4e3b535481e479beabe1ce254f12723040a13db3
zheng <zheng@zheng-Satellite-L510.(none)>
2013-05-11 00:13:02 +0800

    update

--- choice/settings.py
+++ choice/settings.py
@@ -21,13 +21,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'db_choice',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
+        'USER': 'root',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
 
